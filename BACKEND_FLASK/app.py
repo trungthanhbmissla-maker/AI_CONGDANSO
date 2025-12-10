082ff8538e11685e58cdc3337434db4230500cef
trungthanhbmissla-maker <trungthanhbmissla@gmail.com>
2025-12-10 18:09:58 +0700

    Update app.py

--- BACKEND_FLASK/app.py
+++ BACKEND_FLASK/app.py
@@ -372,4 +372,5 @@ def home():
 # 🔟 Chạy server
 # ====================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
